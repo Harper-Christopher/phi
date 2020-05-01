312285dd55cbd00cd113d8d16f714ad44cf6c6e1
Harper-Christopher <christopherharper123@gmail.com>
2020-04-30 22:00:53 -0600

    Update

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,19 @@ function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 	document.getElementsByClassName("menu-icon")[0].classList.toggle("close")
 }
+
+function myFunction() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "inline";
+	  btnText.innerHTML = "Show more"; 
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Show less"; 
+	  moreText.style.display = "inline";
+	}
+  }
